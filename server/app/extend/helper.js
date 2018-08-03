ceaf51aa23b9c1a5732f5f876f4b7c3f73e5305d
niuzz <niuzz@hotmail.com>
2018-08-03 10:03:45 +0800

    2.0 helper-error

--- server/app/extend/helper.js
+++ server/app/extend/helper.js
@@ -15,3 +15,11 @@ exports.success = ({ ctx, res = null, msg = '请求成功' }) => {
   ctx.status = 200;
 };
 
+exports.error = ({ ctx, res = null, msg = '请求失败', code }) => {
+  ctx.body = {
+    code,
+    data: res,
+    msg,
+  };
+  ctx.status = code;
+};
